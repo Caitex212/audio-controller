d5d9ae56b0f151a59e034e0a9b99bcb735eb786f
Caitex212 <firewizard787@gmail.com>
2025-11-13 16:29:13 +0100

    merged the getDefaultDevice

--- AudioController.py
+++ AudioController.py
@@ -15,18 +15,27 @@ class AudioController:
         for source in self.pulse.source_list():
             if source.name == default_source_name:
                 self.source = source
-
-    def getInputDevice(self):
-        return self.source
-
-    def getOutputDevice(self):
-        return self.master
     
-    def getVolume(self, device: pulsectl.pulsectl.PulseSourceInfo): # get the device with getInputDevice or get OutputDevice
+    def refreshDefaults(self):
+        default_sink_name = self.pulse.server_info().default_sink_name
+        default_source_name = self.pulse.server_info().default_source_name
+        for sink in self.pulse.sink_list():
+            if sink.name == default_sink_name:
+                self.master = sink
+        for source in self.pulse.source_list():
+            if source.name == default_source_name:
+                self.source = source
+
+        return default_sink_name, default_source_name
+
+    def getDefaultDevice(self):
+        return self.master, self.source
+    
+    def getVolume(self, device: pulsectl.pulsectl.PulseSourceInfo): # get the device with getDefaultDevice
         return self.pulse.volume_get_all_chans(device)
     
-    def setVolume(self, volume: float, device: pulsectl.pulsectl.PulseSourceInfo): # get the device with getInputDevice or get OutputDevice
+    def setVolume(self, volume: float, device: pulsectl.pulsectl.PulseSourceInfo): # get the device with getDefaultDevice
         self.pulse.volume_set_all_chans(device, volume)
 
-    def setMute(self, mute: bool, device: pulsectl.pulsectl.PulseSourceInfo): # get the device with getInputDevice or get OutputDevice
+    def setMute(self, mute: bool, device: pulsectl.pulsectl.PulseSourceInfo): # get the device with getDefaultDevice
         self.pulse.mute(device, mute)
